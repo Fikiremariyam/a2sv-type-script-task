0e827f7554b4df7a15c22efce7ea8ad6f8faa224
Fikiremariyam <dagi123baba@gmail.com>
2025-04-07 00:33:09 +0300

    add the crud operations

--- src/index.ts
+++ src/index.ts
@@ -1,147 +1,247 @@
-import { create } from 'canvas-confetti';
-import {v4 as uuidV4} from 'uuid'
+  import { create } from 'canvas-confetti';
+  import {v4 as uuidV4} from 'uuid'
 
-// Removed unused import for "canvas-confetti"
+  // Removed unused import for "canvas-confetti"
 
-type Task = {
-  id : string, 
-    title: string, 
-    body: string,
-     completed: boolean, 
-     createdAt: Date
+  type Task = {
+    id : string, 
+      title: string, 
+      body: string,
+      completed: boolean, 
+      createdAt: Date
 
-}
-
-let tasks: Task[] = [];
-localStorage.setItem('Tasks', JSON.stringify(tasks)); // Save updated tasks to localStorage
-
-
-
-
-const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.querySelector('#new-task-form') as HTMLFormElement | null;
-const title = document.querySelector<HTMLInputElement> ("#new-task-title");
-const body = document.querySelector<HTMLInputElement> ("#new-task-body");
-const emptyState = document.querySelector<HTMLDivElement>(".empty-state");
-
-
- form?.addEventListener('submit', (e: SubmitEvent) => {
-  e.preventDefault();
-
-
-  if (title?.value == "" || title?.value == null ) return  
-  if (body?.value == "" || body?.value == null ) return  
-  
-  const newTask ={
-    id :uuidV4(),
-    title: title.value,
-    body : body.value , 
-    completed: false, 
-    createdAt : new Date()
   }
 
-  tasks.push(newTask);
+  let tasks: Task[] = [];
   localStorage.setItem('Tasks', JSON.stringify(tasks)); // Save updated tasks to localStorage
 
-  loadTasks();
-
-  title.value = "";
-  body.value = "";
-
-
- });
 
  
-function loadTasks(){ 
-  console.log(tasks.length);
-  list?.replaceChildren(); 
+  const form = document.querySelector('#new-task-form') as HTMLFormElement | null;
+  const list = document.querySelector<HTMLUListElement>('#list');
+  const title = document.querySelector<HTMLInputElement> ("#new-task-title");
+  const body = document.querySelector<HTMLInputElement> ("#new-task-body");
+  const emptyState = document.querySelector<HTMLDivElement>(".empty-state");
 
-  const taskJSON = localStorage.getItem('Tasks') ;
-  console.log(taskJSON);
 
-  if(taskJSON == null) return []
-  //making the empty state invisible
+  form?.addEventListener('submit', (e: SubmitEvent) => {
+    e.preventDefault();
 
-  
-  if (emptyState) {
-    emptyState.style.display = 'none';
-  }
 
-  const taskes: Task[] = JSON.parse(taskJSON);
-
-  for (let i = 0; i < taskes.length; i++) {
-    console.log(taskes[i].title);
-      showItem(taskes[i]); 
+    if (title?.value == "" || title?.value == null ) return  
+    if (body?.value == "" || body?.value == null ) return  
     
-    
-  }
+    const newTask ={
+      id :uuidV4(),
+      title: title.value,
+      body : body.value , 
+      completed: false, 
+      createdAt : new Date()
+    }
 
-}
+    tasks.push(newTask);
+    localStorage.setItem('Tasks', JSON.stringify(tasks)); // Save updated tasks to localStorage
 
-
-function showItem(task:Task) {
-  console.log("it has invoked the addListItem function");
-  const item = document.createElement('li');
-  item.className= "task-item "
-  const textContainer = document.createElement('div');
-  textContainer.className = "task-container";
-  const buttonContainer = document.createElement('div');
-  buttonContainer.className = "task-button-container";
-
-
-
-  const title = document.createElement('input');
-  title.className = "task-title";
-  title.type = 'text';
-  title.value = task.title;
-  title.readOnly = true;
-  const body = document.createElement('input');
-  body.className = "task-body";
-  body.type = 'text';
-  body.value = task.body;
-  body.readOnly = true;
-  const editButton = document.createElement('button');
-  editButton.className = "task-button";
-  editButton.innerText = "Edit";
-  const deleteButton = document.createElement('button');
-  deleteButton.className = "task-button";
-  deleteButton.innerText = "Delete";
-
-
-  const checkbox = document.createElement('input');
-  
-  checkbox.type = 'checkbox';
-
-
-  textContainer.append(title, body);
-  buttonContainer.append(editButton, deleteButton);
-  item.append(checkbox, textContainer, buttonContainer);
-  list?.append(item);
-
-  checkbox.addEventListener('change', () =>{
-    task.completed= checkbox.checked;//checkbox.checked returns true or false
     loadTasks();
 
+    title.value = "";
+    body.value = "";
+
+
   });
-  deleteButton.addEventListener('click', () => {
-    Delete(task);
-  }
+
+  list?.addEventListener('click', function(e) {
+    const taskItem = (e.target as Element)?.closest('.task-item');
+    //take the closest task to the place where the user clicked
+    
+    if (!taskItem) return;
+    
+    const taskId = taskItem.getAttribute('id');
+
+    
+    
+    // Handle edit button click
+    if ((e.target as Element)?.closest('.btn-edit')) {
+        enableEditMode(taskItem, taskId);
+    }
+    // handle checkbox click
+    if ((e.target as Element)?.closest('input[type="checkbox"]')) {
+      const checkbox = e.target as HTMLInputElement;
+      const task = tasks.find(t => t.id === taskId);
+
+      if (task) {
+        task.completed = !task.completed;
+        localStorage.setItem('Tasks', JSON.stringify(tasks)); // Save updated tasks to localStorage
+        loadTasks();
+      }
+    }
+
+    
+  });
   
-);
+  function loadTasks(){ 
+    
+    list?.replaceChildren(); 
 
-}
+    const taskJSON = localStorage.getItem('Tasks') ;
+    
 
-function Delete(task: Task) {
-  console.log("it has invoked the delete function");
+    if(taskJSON == null) return []
+    //making the empty state invisible
 
-  for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].id == task.id) {
-      tasks.splice(i, 1);
-      localStorage.setItem('Tasks', JSON.stringify(tasks)); // Save updated tasks to localStorage
-      loadTasks();
-      break;
+    
+    if (emptyState) {
+      emptyState.style.display = 'none';
+    }
+
+    const taskes: Task[] = JSON.parse(taskJSON);
+
+    for (let i = 0; i < taskes.length; i++) {
+      
+        showItem(taskes[i]); 
+      
+      
+    }
+
+  }
+
+  function showItem(task:Task) {
+  
+    const item = document.createElement('li');
+    item.id=task.id;
+    item.className= "task-item "
+    const textContainer = document.createElement('div');
+    textContainer.className = "task-container";
+    const buttonContainer = document.createElement('div');
+    buttonContainer.className = "task-button-container";
+
+
+
+    const title = document.createElement('input');
+    title.className = "task-title";
+    title.type = 'text';
+    title.value = task.title;
+    title.readOnly = true;
+    title.style.textDecoration = task.completed ? 'line-through' : 'none';
+    const body = document.createElement('input');
+    body.className = "task-body";
+    body.type = 'text';
+    body.value = task.body;
+    body.readOnly = true;
+    body.style.textDecoration = task.completed ? 'line-through' : 'none';
+    const editButton = document.createElement('button');
+    editButton.className = "task-button btn-edit";
+    editButton.innerText = "Edit";
+    const deleteButton = document.createElement('button');
+    deleteButton.className = "task-button btn-delete";
+    deleteButton.innerText = "Delete";
+
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed; // Set the checkbox state based on task completion
+    
+
+    textContainer.append(title, body);
+    buttonContainer.append(editButton, deleteButton);
+    item.append(checkbox, textContainer, buttonContainer);
+    list?.append(item);
+
+
+    deleteButton.addEventListener('click', () => {
+      Delete(task);
+    }
+
+  );
+
+
+  }
+
+  function Delete(task: Task) {
+    
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].id == task.id) {
+        tasks.splice(i, 1);
+        localStorage.setItem('Tasks', JSON.stringify(tasks)); // Save updated tasks to localStorage
+        loadTasks();
+        break;
+      }
     }
   }
-}
+
+ 
+  function enableEditMode(taskItem :Element, taskId: string | null) {
+
+    const task = tasks.find(t => t.id === taskId);
+
+    const currentTitle= task?.title;
+    const currentBody= task?.body;
+
+    const taskContent = taskItem.querySelector('.task-container');
+
+      // Create edit form
+      const editForm = document.createElement('div');
+      editForm.className = 'task-container';
+
+      const title = document.createElement('input');
+      title.className = "task-title";
+      title.type = 'text';
+      title.value = currentTitle || '';
+      title.readOnly = false;
+      const body = document.createElement('input');
+      body.className = "task-body";
+      body.type = 'text';
+      body.value = currentBody || '';
+      body.readOnly = false;
+
+      editForm.append(title, body);
+      
+      if (taskContent){
+      taskItem.replaceChild(editForm, taskContent);
+      }
+      editForm.focus();
+
+      // Create save 
+      const saveButton = document.createElement('button');
+      saveButton.className = "task-button ";
+      saveButton.innerText = "Save";
+      saveButton.addEventListener('click', () => {
+        
+        for (let i = 0; i < tasks.length;i++){
+          if(tasks[i].id == taskId){
+            tasks[i].title = title.value;
+            tasks[i].body = body.value;
+          }
+
+        } 
+        localStorage.setItem('Tasks', JSON.stringify(tasks)); // Save updated tasks to localStorage
+        loadTasks();
+      }
+      );
+     
+
+      // Create cancel button
+       
+       const cancel = document.createElement('button');
+       cancel.className = "task-button cancel-button";
+       cancel.innerText = "cancel";
+       cancel.addEventListener('click', () => {
+         
+         loadTasks();
+       }
+       );
+
+       const buttonContainer = document.createElement('div');
+       buttonContainer.className = "task-button-container";
+       buttonContainer.append(saveButton,cancel);
 
 
+       //replaceing the buttons with the new buttons
+        const buttonContent = taskItem.querySelector('.task-button-container');
+       if (buttonContent) {
+           taskItem.replaceChild(buttonContainer , buttonContent);
+       }
+
+
+
+  }
